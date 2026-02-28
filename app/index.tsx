8c49d392eb3fe681a51bd3d2daa5ef3e39e7a9e4
IDstructive <obrechon@gmail.com>
2026-02-28 17:20:03 +0100

    NewWound

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { View, ScrollView, TouchableOpacity, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Hand, CircleCheck, CircleX, CircleHelp } from 'lucide-react-native';
-import { Button } from '@/components/ui/button';
+import { useRouter } from 'expo-router';
 import { WoundCard } from '@/components/wound-card';
 
 import { Wounds } from '@/data/Wounds';
 
 export default function HomeScreen() {
+  const router = useRouter();
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <Text className="text-3xl font-light text-green-400 text-center mt-5 mb-6">
@@ -21,6 +22,7 @@ export default function HomeScreen() {
         <TouchableOpacity
           className="flex-row items-center bg-green-200 px-6 py-4 rounded-full gap-2"
           activeOpacity={0.8}
+          onPress={() => router.push('/new-wound')}
         >
           <Text className="text-xl">✊</Text>
           <Text className="text-lg font-semibold text-gray-800">Analyze a wound</Text>
---
+++ app/new-wound/index.tsx
@@ -0,0 +1,10 @@
+import { View, ScrollView, TouchableOpacity, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function NewWound() {
+    return (
+      <SafeAreaView className="flex-1 bg-white">
+       <Text>New Wound</Text>
+      </SafeAreaView>
+    );
+  }
